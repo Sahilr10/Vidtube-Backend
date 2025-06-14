b4115aa1672224949bc3e2033fb1d1a56d6dbd89
Sahil Raut <rautsahil102005@gmail.com>
2025-06-14 22:20:33 +0530

    add subscription model

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { logoutUser,loginUser,registerUser, refreshAccessToken } from '../controllers/user.controller.js';
+import { logoutUser, loginUser, registerUser, refreshAccessToken } from '../controllers/user.controller.js';
 import { upload } from '../middlewares/multer.middleware.js'
 import { verifyJWT } from '../middlewares/auth.middleware.js';
 const router = Router();
